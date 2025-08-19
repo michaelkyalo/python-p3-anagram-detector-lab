248c0b1118a95048e53465cbffb302f28d885fe1
michaelkyalo <michaelkyalo876@gmail.com>
2025-08-19 11:59:04 +0300

    cobra

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        """Return list of words that are anagrams of self.word"""
+        return [w for w in words if sorted(w.lower()) == sorted(self.word) and w.lower() != self.word]
